4e91d17a90808e1efddb0880fa8a2f1e5c4156cc
gabrielstuff <gabriel@soixantecircuits.fr>
2015-02-02 22:28:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Testio  
+
+Testio is an italian fetcher. He fetch periodically pictures from instagram.
+
+That is all...
+
+## Install
+
+`npm install`
+
+### using instagram
+
+`cp config.example.json config.json`
+
+Then edit the `config.json` file
+
+## Run
+
+`npm start`
+
---
+++ index.js
@@ -0,0 +1,56 @@
+var ig = require('instagram-node').instagram(),
+  fs = require('fs'),
+  request = require('request'),
+  Reminder = require('reminder'),
+  remind = new Reminder(),
+  mkdirp = require('mkdirp'),
+  config = require('./config.json'),
+  tagArray = ['cow','cool','beauty','love'];
+// Every call to `ig.use()` overrides the `client_id/client_secret`
+// or `access_token` previously entered if they exist.
+//ig.use({ access_token: 'YOUR_ACCESS_TOKEN' });
+ig.use({
+  client_id: config.client_id,
+  client_secret: config.client_secret
+});
+
+
+    
+mkdirp('./tmp/', function (err) {
+    if (err) console.error(err)
+    else console.log('pow!')
+});
+
+//ig.location_search({ lat: 48.565464564, lng: 2.34656589 }, [options,] function(err, result, remaining, limit) {});
+
+var getRandom = function(arr){
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+var searchAndSave = function(tag) {
+  ig.tag_media_recent(tag, function(err, medias, pagination, remaining, limit) {
+    //ig.media_search(48.4335645654, 2.345645645, function(err, medias, remaining, limit) {
+    if (err) {
+      console.log(err);
+      return -1;
+    }
+    var medias = getRandom(medias),
+      image = medias.images.standard_resolution.url;
+    console.log(image);
+
+    var ws = fs.createWriteStream('./tmp/' + tag + '_' + Date.now() + '.jpg');
+    ws.on('error', function(err) {
+      console.log(err);
+    });
+    request(image).pipe(ws);
+  });
+}
+
+remind.every('minute', function(date) {
+    var selectedTag = getRandom(tagArray);
+    console.log('Fetch: ', selectedTag);
+    searchAndSave(selectedTag);    
+});
+
+searchAndSave('love');    
+console.log('Running...');
