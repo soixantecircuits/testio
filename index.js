5bae56771d3f6ff139beb4bef52d167b3405f729
hugo <hugo@soixantecircuits.fr>
2015-04-02 14:42:41 +0200

    small improvments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var ig = require('instagram-node').instagram(),
   config = require('./config.json'),
   Repeat = require('repeat'),
   tagArray = ['cow','cool','beauty','love'];
+
 // Every call to `ig.use()` overrides the `client_id/client_secret`
 // or `access_token` previously entered if they exist.
 //ig.use({ access_token: 'YOUR_ACCESS_TOKEN' });
@@ -15,14 +16,21 @@ ig.use({
   client_secret: config.client_secret
 });
 
-
-
 mkdirp('./tmp/', function (err) {
-    if (err) console.error(err)
-    else console.log('pow!')
+  if(err){
+    console.error(err);
+  }else{
+    console.log('pow!');
+  }
 });
 
-//ig.location_search({ lat: 48.565464564, lng: 2.34656589 }, [options,] function(err, result, remaining, limit) {});
+mkdirp('./tmp/' + config.namespace, function (err) {
+  if(err){
+    console.error(err);
+  }else{
+    console.log('working on namespace:', config.namespace);
+  }
+});
 
 var getRandom = function(arr){
   return arr[Math.floor(Math.random() * arr.length)]
@@ -30,30 +38,32 @@ var getRandom = function(arr){
 
 var searchAndSave = function(tag) {
   ig.tag_media_recent(tag, function(err, medias, pagination, remaining, limit) {
-    //ig.media_search(48.4335645654, 2.345645645, function(err, medias, remaining, limit) {
     if (err) {
       console.log(err);
       return -1;
     }
-    var medias = getRandom(medias),
-      image = medias.images.standard_resolution.url;
+    var medias = getRandom(medias);
+    var image = medias.images.standard_resolution.url;
     console.log(image);
 
     var ws = fs.createWriteStream('./tmp/' + config.namespace + '/' + tag + '_' + Date.now() + '.jpg');
-    ws.on('error', function(err) {
+    ws.on('error', function (err) {
       console.log(err);
     });
     request(image).pipe(ws);
   });
 }
 
-remind.every('minute', function(date) {
-    var selectedTag = getRandom(tagArray);
-    console.log('Fetch: ', selectedTag);
-    Repeat(function(){
-      searchAndSave(selectedTag);
-    }).every(450, 'ms').for(32, 's').start.now();
+function triggerSearchLoop(){
+  var selectedTag = getRandom(tagArray);
+  console.log('Searching tag:', selectedTag);
+  Repeat(function(){
+    searchAndSave(selectedTag);
+  }).every(1, 's').for(59, 's').start.now();
+}
+
+remind.every('minute', function (date) {
+  triggerSearchLoop();
 });
 
-searchAndSave('love');
-console.log('Running...');
+triggerSearchLoop();
